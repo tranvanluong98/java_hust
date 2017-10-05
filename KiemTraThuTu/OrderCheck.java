a8cb88ec3e06de6f614f050d5ed3b8ee667f5c1a
LuongBEAT <luong98svtybg@gmail.com>
2017-10-05 15:03:02 +0700

    Order Check

---
+++ KiemTraThuTu/OrderCheck.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class OrderCheck{
+	public static void main(String[] args) {
+		Scanner ip =  new Scanner(System.in);
+		System.out.println("Ban hay nhap so thu nhat ");
+		int a = ip.nextInt();
+		System.out.println("Ban hay nhap so thu hai ");
+		int b = ip.nextInt();
+		System.out.println("Ban hay nhap so thu ba ");
+		int c = ip.nextInt();
+		 if((a< b&&b<c)||(a>b&&b>c)){
+            System.out.println("True");
+        }else 
+            System.out.println("False");
+	}
+}
